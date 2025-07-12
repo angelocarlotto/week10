5ed06cd5ba822685d7073c0bd45907951c8a186d
Angelo Carlotto <angelocarlotto@gmail.com>
2025-07-12 00:01:52 -0400

    implemented email and password login

--- app/src/main/java/com/example/week10/MainActivity.kt
+++ app/src/main/java/com/example/week10/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.week10
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -13,14 +14,16 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.firebase.Firebase
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.GoogleAuthProvider
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
-
-    private lateinit var googleSignInClient: GoogleSignInClient
     private lateinit var auth: FirebaseAuth
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var googleSignInClient: GoogleSignInClient
+
     private val tracking_nb_google_sing_up = 9876
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -64,14 +67,70 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.buttonSingupFacebook.setOnClickListener {
-
+            Toast.makeText(this, "Facebook login/singup", Toast.LENGTH_SHORT).show()
         }
 
         binding.buttonFaceBookLogout.setOnClickListener {
+            Toast.makeText(this, "Facebook ", Toast.LENGTH_SHORT).show()
+        }
 
+        binding.buttonSingupPasswordBased.setOnClickListener {
+            val email=binding.editTextText.text.toString()
+            val password=binding.editTextTextPassword.text.toString()
+            auth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d(TAG, "createUserWithEmail:success")
+                        val user = auth.currentUser
+                        updateUI(user)
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w(TAG, "createUserWithEmail:failure", task.exception)
+                        Toast.makeText(
+                            baseContext,
+                            "Authentication failed.",
+                            Toast.LENGTH_SHORT,
+                        ).show()
+                        updateUI(null)
+                    }
+                }
+        }
+
+        binding.buttonPasswordBasedLogin.setOnClickListener {
+            val email=binding.editTextText.text.toString()
+            val password=binding.editTextTextPassword.text.toString()
+            auth.signInWithEmailAndPassword(email,password)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        val user = auth.currentUser
+                        updateUI(user)
+                    }else{
+                        updateUI(null)
+                    }
+                }
         }
     }
+    companion object {
+        private const val TAG = "EmailPassword"
+    }
+    private fun sendEmailVerification() {
+        // [START send_email_verification]
+        val user = auth.currentUser!!
+        user.sendEmailVerification()
+            .addOnCompleteListener(this) { task ->
+                // Email Verification sent
+            }
+        // [END send_email_verification]
+    }
 
+    private fun updateUI(user: FirebaseUser?) {
+        if (user==null){
+            Toast.makeText(this, "error on singup ", Toast.LENGTH_SHORT).show()
+        }else{
+            Toast.makeText(this, "Sucess ${user.uid} ", Toast.LENGTH_SHORT).show()
+        }
+    }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
         if (requestCode == tracking_nb_google_sing_up) {
